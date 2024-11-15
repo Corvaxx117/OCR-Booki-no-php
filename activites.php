3ebd64f597e26f3b8031be28085a36a736144c9c
Corvaxx117 <julien.amiel117@gmail.com>
2024-11-15 12:28:30 +0100

    version php

---
+++ activites.php
@@ -0,0 +1,25 @@
+<?php
+// Tableau contenant les détails de chacun des hebergements proposés (id, titre, infos, lien de l'image)
+$activites = [
+    1 => [
+        'id' => 1,
+        'titre' => 'Vieux-Port',
+        'image' => 'images/activites/reno-laithienne.jpg',
+    ],
+    2 => [
+        'id' => 2,
+        'titre' => 'Fort de Pomègues',
+        'image' => 'images/activites/paul-hermann.jpg',
+    ],
+    3 => [
+        'id' => 3,
+        'titre' => 'Parc national des Calanques',
+        'image' => 'images/activites/kilyan-sockalingum.jpg',
+
+    ],
+    4 => [
+        'id' => 4,
+        'titre' => 'Notre-Dame-de-la-Garde',
+        'image' => 'images/activites/florian-wehde.jpg',
+    ]
+];
